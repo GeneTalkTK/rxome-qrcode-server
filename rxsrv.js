86ac6a0c792893ae34d34d8cefac62dfdbb27989
Tom <tom@kamphans.de>
2023-09-16 14:34:53 +0200

    Began windows service

--- rxsrv.js
+++ rxsrv.js
@@ -1,35 +1,57 @@
-const express = require('express');
-const coder = require('./node_modules/rxome-generator/lib/rxome-generator');
-const RxAPI = require( './node_modules/rxome-generator/lib/rxome-api' );
-const RxAPIDemo = require( './node_modules/rxome-generator/lib/rxome-api-demo' );
-const bodyParser = require('body-parser');
+#!/usr/bin/env node
 
-const app = express();
+import express from 'express';
 
-const qrApi = RxAPI.TESTAPI
+import * as FS from 'fs' ;
+import { program } from 'commander';
+import bodyParser from 'body-parser';
 
-const port = 3000;
+import * as coder from 'rxome-generator';
 
-app.use((req, res, next) => {
+const server = express();
+
+const qrApi = coder.TESTAPI
+
+// const version = require('./package').version
+import * as pkg from './package.json' assert { type: 'json' };
+const version=pkg.default.version;
+
+let port = 1607;
+let apiId = '';
+let apiKey = '';
+
+/************************************************************************************
+ * set up express.js
+ ************************************************************************************/
+
+server.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
-app.use(bodyParser.json());
+server.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('This is the RxOME Qrcode generator API');
+/************************************************************************************
+ * express.js middleware, API entry points
+ ************************************************************************************/
+
+server.get('/', (req, res) => {
+  res.send(`This is the RxOME QRcode generator API Version ${version} for lab id ${apiId} running on port ${port} with PID ${process.pid}\n`);
 });
 
-app.post('/qrcode', async (req, res) => {
+server.get('/demo', (req, res) => {
+  res.download('./demo_data_full.json');
+});
+
+server.post('/img', async (req, res) => {
   const qrData = req.body;
   
   // set demo credentials:
   ('credentials' in qrData) || (qrData.credentials = {});
-  qrData.credentials.keyId = RxAPIDemo.DEMO_API_ID;
-  qrData.credentials.key = RxAPIDemo.DEMO_API_PRIVATE_KEY;
+  qrData.credentials.keyId = coder.DEMO_API_ID;
+  qrData.credentials.key = coder.DEMO_API_PRIVATE_KEY;
   qrData.credentials.user = 'info@rxome.net';
   delete qrData.credentials.keyFile;
   
@@ -43,12 +65,91 @@ app.post('/qrcode', async (req, res) => {
   res.writeHead(200, {
     'Content-Type': 'image/png',
     'Content-Length': imgData.length
- });
+  });
 
  res.end(imgData);
 });
 
+server.post('/', async (req, res) => {
+  const qrData = req.body;
+  
+  // set demo credentials:
+  ('credentials' in qrData) || (qrData.credentials = {});
+  qrData.credentials.keyId = coder.DEMO_API_ID;
+  qrData.credentials.key = coder.DEMO_API_PRIVATE_KEY;
+  qrData.credentials.user = 'info@rxome.net';
+  delete qrData.credentials.keyFile;
+  
+  // generate QR Code
+  const result = await coder.makeQR( qrData, qrApi );
+  const data = { 
+    qr_code: result.qr_code,
+    pseudonym: result.pseudonym
+  }
+  const datastr = JSON.stringify( data );
 
-app.listen(port, () => {
-  console.log(`RxOME QR Code generator listening at http://localhost:${port}`);
+  // send QR Code
+  res.writeHead(200, {
+    'Content-Type': 'application/json',
+    'Content-Length': datastr.length
+  });
+
+ res.end( datastr );
 });
+
+/************************************************************************************
+ * use commander to parse command line
+ ************************************************************************************/
+
+program
+  .name('rxsrv')
+  .usage('usage: rxsrv -i <id> (-e | -c <cfg_file> | -k <key_file> | -s <key>) [-p <port>]')
+  .description(
+`Start the QR-code tool as service listening on localhost:<port>.
+Before first use, please generate an API access key with rxcode and deposit the public key on the RxOME server.\n
+The command-line parameters -k, -s, -p precede the environment variables (if -e specified), which, in turn, precede the config file (if -c is also specified). A key string (-s) has precedence over a key from a key file (-k).
+`)
+  .version('1.0.0')
+  .addHelpText('beforeAll', 'RxOME QR-Code generation server\n')
+  .addHelpText('afterAll', '\nAuthor: Tom Kamphans, GeneTalk GmbH, 2023')
+  .option('-c, --config <filename>', 'JSON file with config, entries id, key, [port]; -c-- to read from stdin')
+  .option('-e, --environment', 'use environment variables RXID, RXKEY, RXPORT to configure rxsrv (useful for working with docker)')
+  .option('-i, --keyId <id>', 'API access ID')
+  .option('-k, --keyFile <filename>', 'Filename with API access key (default: use -s)')
+  .option('-s, --key <key string>', 'API access key')
+  .option('-p, --port <port>', 'Set port for server, default: 1607')
+  .action( (options) => {
+    if ( options.config ) {
+      const cfg = JSON.parse( FS.readFileSync( options.config === '--' ? '/dev/stdin' : options.config ));
+      apiId = cfg.id;
+      apiKey = cfg.key;
+      cfg.port && (port = +cfg.port);
+    }
+
+    if ( options.environment ) {
+      process.env.RXID   && ( apiId  =  process.env.RXID   );
+      process.env.RXKEY  && ( apiKey =  process.env.RXKEY  );
+      process.env.RXPORT && ( port   = +process.env.RXPORT );
+    }
+    
+    if ( options.keyFile ) {
+      apiKey = RxAPI.readSigKey( options.keyFile );
+    }
+
+    options.keyId && ( apiId  =  options.keyId );
+    options.key   && ( apiKey =  options.key   );
+    options.port  && ( port   = +options.port  );
+
+    if ( !apiId || !apiKey ) {
+      console.log('Error: keyID and either keyFile or key must be given!');
+      return 1;
+    }
+
+    server.listen(port, () => {
+      console.log(`RxOME QR Code generator listening at http://localhost:${port}`);
+    });
+  })
+
+
+
+program.parse();
---
+++ rxsrv_win.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+import os from 'os';
+if ( os.platform().indexOf('win32') < 0) {
+  throw 'rxsrv_win is supported on Windows only.';
+}
+
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name:'Hello World',
+  description: 'The nodejs.org example web server.',
+  script: 'C:\\path\\to\\helloworld.js',
+  nodeOptions: [
+    '--harmony',
+    '--max_old_space_size=4096'
+  ]
+  //, workingDirectory: '...'
+  //, allowServiceLogon: true
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('install',function(){
+  svc.start();
+});
+
+svc.install();
